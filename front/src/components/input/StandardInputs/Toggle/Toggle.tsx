7d1fe36d6475b33c214569fb084749e94a0a2610
Sabalot <s.sabalot@gmail.com>
2022-04-07 13:16:08 +0200

    change display result api

--- front/src/components/input/StandardInputs/Toggle/Toggle.tsx
+++ front/src/components/input/StandardInputs/Toggle/Toggle.tsx
@@ -42,7 +42,7 @@ export const Toggle: React.FC<IToggleOptions> = ({
       }}
       className={classNames(
         checked ? 'bg-indigo-600' : 'bg-gray-200',
-        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 p-0.5 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
       )}
     >
       <span className="sr-only">Use setting</span>
