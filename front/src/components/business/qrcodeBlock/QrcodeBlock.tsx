824229cef988ae49938e542f8654a7503e5eeded
Sabalot <s.sabalot@gmail.com>
2022-07-08 15:31:07 +0200

    fix display qr code

--- front/src/components/business/qrcodeBlock/QrcodeBlock.tsx
+++ front/src/components/business/qrcodeBlock/QrcodeBlock.tsx
@@ -9,7 +9,7 @@ interface SummarySimulatorProps {}
 export const QrcodeBlock: React.FC<SummarySimulatorProps> = () => {
   return (
     <div className="flex flex-row rounded-xl border border-secondary-600 p-4">
-      <div className="h-16">
+      <div className="h-16 w-16">
         <QRCodeSVG value="https://www.douane.gouv.fr/" size={64} />
       </div>
       <div className="ml-4 flex flex-col gap-2">
