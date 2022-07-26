22311a650869b47da50701893416508e471176c8
Sabalot <s.sabalot@gmail.com>
2022-07-26 17:20:11 +0200

    fix bug display img

--- front/src/components/business/underConstruction/UnderConstruction.tsx
+++ front/src/components/business/underConstruction/UnderConstruction.tsx
@@ -7,7 +7,7 @@ import underConstruction from '@/assets/images/Under-Construction.png';
 export const UnderConstruction: React.FC = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center text-center">
-      <div className="w-60">
+      <div className="h-auto w-60">
         <Image src={underConstruction} />
       </div>
       <p className="-mt-8">
