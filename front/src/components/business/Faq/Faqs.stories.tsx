4d449c0a2a03cf103486f1920081c2ac20229268
Sabalot <s.sabalot@gmail.com>
2022-10-01 22:57:03 +0200

    fix build front

--- front/src/components/business/Faq/Faqs.stories.tsx
+++ front/src/components/business/Faq/Faqs.stories.tsx
@@ -1,16 +1,19 @@
 import { Meta } from '@storybook/react';
 
 import { Faqs } from './Faqs';
+import { BlocElements } from '@/staticData';
 
 export default {
   title: 'Components/Business/Faqs',
   component: Faqs,
 } as Meta;
 
+const searchData: BlocElements[] = [];
+
 export const withVariant = (): JSX.Element => (
   <div className="p-3">
     <br />
-    <Faqs />
+    <Faqs faqData={searchData} />
     <br />
   </div>
 );
