912f990c8bfce7c7c9270facec742041c9315cb6
Anthony HOANG <anthonyhoang@MacBook-Pro-de-Anthony.local>
2023-02-22 08:41:55 +0100

    Update component design

--- front/src/components/business/ActualityCard/ActualityCard.stories.tsx
+++ front/src/components/business/ActualityCard/ActualityCard.stories.tsx
@@ -7,6 +7,11 @@ const meta: Meta<typeof ActualityCard> = {
   component: ActualityCard,
 };
 
+const todayDate = new Date();
+const todayMilliseconds = todayDate.getTime();
+const beforeMilliseconds = todayMilliseconds - 432000000;
+const beforeDate = new Date(beforeMilliseconds);
+
 export default meta;
 type Story = StoryObj<typeof ActualityCard>;
 
@@ -19,19 +24,18 @@ export const Playground: Story = {
   },
 };
 
-const ACTUALITYCARD_DATA = {
-  title: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
-  creationDate: new Date(),
-  content:
-    'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-};
-
 export const Base = () => (
   <div>
     <ActualityCard
-      title={ACTUALITYCARD_DATA.title}
-      creationDate={ACTUALITYCARD_DATA.creationDate}
-      content={ACTUALITYCARD_DATA.content}
+      title="CreationDate < 5 jours"
+      creationDate={new Date()}
+      content="Une ActualityCard récente (moins de 5 jours) le temps écoulé depuis la date de création. Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet ut perspiciatis unde omnis iste natus."
+    />
+    <br />
+    <ActualityCard
+      title="CreationDate ≥ 5 jours"
+      creationDate={beforeDate}
+      content="Une ActualityCard datant de plus de 5 jours affichera la date de création absolue au lieu du temps écoulé depuis la date de création. Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet."
     />
   </div>
 );
--- front/src/components/business/ActualityCard/ActualityCard.tsx
+++ front/src/components/business/ActualityCard/ActualityCard.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
+import dayjs from 'dayjs';
+
 import { Typography } from '@/components/common/Typography';
+
+const relativeTime = require('dayjs/plugin/relativeTime');
+
+dayjs.extend(relativeTime);
+
+require('dayjs/locale/fr');
+
+dayjs.locale('fr');
 
 export type ActualityCardProps = {
   title: string;
@@ -15,7 +25,8 @@ export const getValueOrEmpty = (text: string) => {
   return text;
 };
 
-const maxTextLength = 50;
+const maxTextLength = 150;
+const millisecondsLimit = 432000000;
 
 const splitDescription = (text: string) => {
   const sliceDescription = getValueOrEmpty(text).slice(0, maxTextLength);
@@ -26,20 +37,32 @@ const splitDescription = (text: string) => {
   return text;
 };
 
-const dateFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-
 export const ActualityCard = ({ title, creationDate, content }: ActualityCardProps) => {
   const splitContent = splitDescription(content);
   return (
-    <div className="grid h-[200px] w-[300px] grid-rows-[40px_30px_1fr] bg-gray-300 p-6">
-      <Typography weight="bold" size="text-xl" ellipsis>
+    <div className="grid h-[190px] w-[380px] grid-rows-[25px_40px_1fr] rounded-xl bg-gray-100 px-5 py-3">
+      <div className="grid h-full w-full grid-cols-2">
+        <span>
+          <Typography color="middle-gray" size="text-sm">
+            Nomenclature
+          </Typography>
+        </span>
+        <span>
+          <Typography color="middle-gray" size="text-sm" textPosition="text-right">
+            {[
+              new Date().getTime() - creationDate.getTime() < millisecondsLimit &&
+                dayjs().to(dayjs(creationDate)),
+              new Date().getTime() - creationDate.getTime() >= millisecondsLimit &&
+                dayjs(creationDate).format('DD/MM/YYYY'),
+            ]}
+          </Typography>
+        </span>
+      </div>
+      <Typography weight="bold" size="text-2xl" color="black" ellipsis>
         {title}
       </Typography>
-      <Typography variant="paragraph" size="text-lg">
-        {creationDate.toLocaleDateString(undefined, dateFormatOptions)}
-      </Typography>
-      <span className="mt-3">
-        <Typography variant="paragraph" size="text-lg">
+      <span>
+        <Typography variant="paragraph" size="text-base" color="black">
           {splitContent}
         </Typography>
       </span>
