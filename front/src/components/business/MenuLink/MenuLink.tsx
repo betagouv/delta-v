37a621ada22fc95eec7700ac55bccd556e37b616
Anthony HOANG <anthonyhoang@MacBook-Pro-de-Anthony.local>
2023-02-24 10:24:13 +0100

    Adjust to tailwindCSS default values

--- front/src/components/business/MenuLink/MenuLink.tsx
+++ front/src/components/business/MenuLink/MenuLink.tsx
@@ -15,7 +15,7 @@ export type MenuLinkProps = {
 export const MenuLink = ({ title, to, badgeNumber, iconName }: MenuLinkProps) => {
   return (
     <Link href={to}>
-      <a className="grid h-[40px] w-[210px] grid-cols-[34px_1fr_20px] content-center rounded-lg border border-black">
+      <a className="grid h-10 w-52 grid-cols-[34px_1fr_20px] content-center rounded-lg border border-black">
         <div className="flex items-center justify-self-center">
           {badgeNumber && (
             <div className="h-5 w-5 rounded-full bg-[#EFB45A] text-center">
