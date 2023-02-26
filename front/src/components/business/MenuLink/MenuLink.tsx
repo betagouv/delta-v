938e1e778a6a0c0699712cdb02a9fc4ba8136994
Touan95 <anthony.hoang@snowpact.com>
2023-02-26 09:34:40 +0100

    Feature/375/link component (#398)
    * WIP:Design + link to do
    
    * WIP:fix to do
    
    * Fix using grid instead of flexbox
    
    * Fix using grid instead of flexbox + optimization
    
    * Fix code review
    
    * Fix code review
    
    * Adjust to tailwindCSS default values
    
    * Fix code review
    
    ---------
    
    Co-authored-by: Anthony HOANG <anthonyhoang@MacBook-Pro-de-Anthony.local>

---
+++ front/src/components/business/MenuLink/MenuLink.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Link from 'next/link';
+
+import { Icon } from '@/components/common/Icon';
+import { Typography } from '@/components/common/Typography';
+
+export type MenuLinkProps = {
+  title: string;
+  to: string;
+  badgeNumber?: Number;
+  iconName?: string;
+};
+
+export const MenuLink = ({ title, to, badgeNumber, iconName }: MenuLinkProps) => {
+  return (
+    <Link href={to}>
+      <a className="grid h-10 w-52 grid-cols-[34px_1fr_20px] content-center rounded-lg border border-black">
+        <div className="flex items-center justify-self-center">
+          {badgeNumber !== undefined && (
+            <div className="h-5 w-5 rounded-full bg-[#EFB45A] text-center">
+              <Typography color="black" size="text-xs" lineHeight="leading-5">
+                {badgeNumber}
+              </Typography>
+            </div>
+          )}
+          {badgeNumber === undefined && iconName && <Icon name={iconName} size="lg" />}
+        </div>
+        <Typography color="black">{title}</Typography>
+        <div className="flex items-center justify-self-center">
+          <Icon name="chevron-thin-right" size="base" />
+        </div>
+      </a>
+    </Link>
+  );
+};
