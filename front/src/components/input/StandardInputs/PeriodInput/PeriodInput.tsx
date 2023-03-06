30c84ffa758b7f4ccf6c66c86c13e281df878d9b
farid <farid.boussetta.95@gmail.com>
2023-03-06 19:00:58 +0100

    feature(#389): period input

--- front/src/components/input/StandardInputs/PeriodInput/PeriodInput.tsx
+++ front/src/components/input/StandardInputs/PeriodInput/PeriodInput.tsx
@@ -2,22 +2,28 @@ import React from 'react';
 
 export interface IPeriodInputOptions {
   register?: any;
+  startDateInputId?: string;
+  endDateInputId?: string;
 }
 
-export const PeriodInput: React.FC<IPeriodInputOptions> = ({ register }: IPeriodInputOptions) => (
+export const PeriodInput: React.FC<IPeriodInputOptions> = ({
+  register,
+  startDateInputId,
+  endDateInputId,
+}: IPeriodInputOptions) => (
   <>
     <div className="flex flex-row overflow-hidden rounded-full">
       <input
         type="date"
         className="flex-1 justify-center rounded-l-full border-2 border-r"
         placeholder="Du: jj/mm/aaaa"
-        {...register('startDate', { valueAsDate: true })}
+        {...register(startDateInputId, { valueAsDate: true })}
       />
       <input
         type="date"
         className="flex-1 rounded-r-full border-2 border-l border-secondary-500"
         placeholder="Au: jj/mm/aaaa"
-        {...register('endDate', { valueAsDate: true })}
+        {...register(endDateInputId, { valueAsDate: true })}
       />
     </div>
   </>
