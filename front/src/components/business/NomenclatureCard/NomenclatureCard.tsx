2b1e9379f702b31180ce6638b0300f8862ff52d7
Anthony HOANG <anthonyhoang@MBP-de-Anthony.lan>
2023-03-09 13:46:21 +0100

    Rebasing

--- front/src/components/business/NomenclatureCard/NomenclatureCard.tsx
+++ front/src/components/business/NomenclatureCard/NomenclatureCard.tsx
@@ -34,10 +34,10 @@ export const NomenclatureCard = ({
   return (
     <div
       className={cs(
-        'relative grid rounded-xl border border-gray-200 p-5 grid-cols-[40px_1fr] w-64 h-28',
+        'relative grid rounded-xl border border-gray-200 p-3 grid-cols-[40px_1fr] w-64 h-28',
       )}
     >
-      <div className="h-7 w-7 bg-gray-200">X</div>
+      <div className="h-7 w-7 bg-gray-200" />
       <div className="grid-rows-[1fr_3fr]">
         <div className="flex gap-3">
           {product.nomenclatures &&
@@ -47,10 +47,12 @@ export const NomenclatureCard = ({
               </Typography>
             ))}
         </div>
-        <div className="w-fit">Manteaux, vestes, cabans, capes, anoraks, blousons</div>
+        <div className="line-clamp-3">
+          <Typography color="black">{product.relatedWords.join(', ')}</Typography>
+        </div>
       </div>
       <div
-        className="absolute top-2.5 right-2.5 h-4 w-4 cursor-pointer text-[#5A7BF0]"
+        className="absolute top-2 right-2 h-4 w-4 cursor-pointer text-[#5A7BF0]"
         onClick={handleClick}
       >
         {isFavorite ? <Icon name="star-full" /> : <Icon name="star-empty" color="grey" />}
