40ec85dc3e1524de2c9527f9d5c3ac1430e10b22
Stéphane Sabalot <sabalot@OrdinatStephane.lan>
2023-05-05 17:21:34 +0200

    validate email endpoint

--- back/src/api/authentication/index.ts
+++ back/src/api/authentication/index.ts
@@ -2,6 +2,11 @@ import { Router } from 'express';
 import { login } from './login';
 import { register } from './register';
 import { validateEmail } from './validateEmail';
+<<<<<<< HEAD
 import { askEmailValidation } from './askEmailValidation';
 
 export default Router().use(login).use(register).use(validateEmail).use(askEmailValidation);
+=======
+
+export default Router().use(login).use(register).use(validateEmail);
+>>>>>>> 7972cd9 (validate email endpoint)
