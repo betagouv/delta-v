91a7f8f8a0acc61a411a7c2de08c2f1d3fbc3fc7
farid <farid.boussetta.95@gmail.com>
2023-06-07 00:37:59 +0200

    feat(#551): create queryClient

--- front/src/stores/declaration/useCase.store.ts
+++ front/src/stores/declaration/useCase.store.ts
@@ -113,51 +113,6 @@ export const createUseCaseDeclarationSlice: StoreSlice<DeclarationUseCaseSlice> 
       });
     }
   },
-  validateDeclaration: async (declarationId: string) => {
-    try {
-      const declarationData = get().declaration.appState;
-      const data = {
-        shoppingProducts: declarationData.declarationRequest.shoppingProducts.map(
-          (shoppingProduct: ShoppingProduct) => ({
-            id: shoppingProduct.productId,
-            customName: shoppingProduct.name,
-            customId: shoppingProduct.id,
-            originalValue: shoppingProduct.value,
-            currency: shoppingProduct.currency,
-          }),
-        ),
-        border: declarationData.declarationRequest.border,
-        age: declarationData.declarationRequest.contactDetails.age,
-        country: declarationData.declarationRequest.meansOfTransportAndCountry.country,
-        meanOfTransport:
-          declarationData.declarationRequest.meansOfTransportAndCountry.meansOfTransport,
-        authorEmail: declarationData.declarationRequest.contactDetails.email,
-        authorId: 'fc3aed41-e6f2-4937-aa09-7113c1641014',
-        authorFullName: `${declarationData.declarationRequest.contactDetails.firstName} ${declarationData.declarationRequest.contactDetails.lastName}`,
-        authorType: 'agent',
-        declarantAddressStreet: declarationData.declarationRequest.contactDetails.address,
-        declarantAddressCity: declarationData.declarationRequest.contactDetails.city,
-        declarantAddressPostalCode: declarationData.declarationRequest.contactDetails.postalCode,
-        declarantPhoneNumber: declarationData.declarationRequest.contactDetails.phoneNumber,
-        declarantEmail: declarationData.declarationRequest.contactDetails.email,
-        declarantFirstName: declarationData.declarationRequest.contactDetails.firstName,
-        declarantLastName: declarationData.declarationRequest.contactDetails.lastName,
-      };
-      const response = (await axios.put(`/api/declaration/${declarationId}`, data))
-        .data as DeclarationResponse;
-      set((state: any) => {
-        const newState = { ...state };
-        newState.declaration.appState.validateDeclarationResponse = response;
-        return newState;
-      });
-    } catch (error: any) {
-      set((state: any) => {
-        const newState = { ...state };
-        newState.declaration.appState.error = error?.response?.data;
-        return newState;
-      });
-    }
-  },
   addProductCartDeclaration: (shoppingProduct: ShoppingProduct): void => {
     set((state: any) => {
       const newState = { ...state };
