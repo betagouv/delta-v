d46f0836f5245afb4e6544260a16bab9174e8bef
farid <farid.boussetta.95@gmail.com>
2023-06-06 18:30:41 +0200

    feat(#550): create and get declaration

--- back/src/api/declaration/putDeclaration/services/publicId.service.ts
+++ back/src/api/declaration/putDeclaration/services/publicId.service.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { customAlphabet } from 'nanoid/async';
+import { customAlphabet } from 'nanoid';
 
-export const generatePublicId = async (): Promise<string> => {
+export const generatePublicId = (): string => {
   const nanoid = customAlphabet('1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ', 21);
-  return await nanoid();
+  return nanoid();
 };
