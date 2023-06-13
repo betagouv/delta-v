8c5828ee983826dafce8ee0b43f8aa67931b658d
farid <farid.boussetta.95@gmail.com>
2023-06-13 17:41:56 +0200

    feat(#551): create queryClient: security fix

--- front/src/pages/agent/declaration/[id].tsx
+++ front/src/pages/agent/declaration/[id].tsx
@@ -16,7 +16,7 @@ import { MainAgent } from '@/templates/MainAgent';
 
 const DeclarationSearch = () => {
   const router = useRouter();
-  const { id } = router.query as { id: string };
+  const id = 'id' in router.query && typeof router.query.id === 'string' ? router.query.id : '';
 
   const { isLoading, data: validateDeclarationResponse } = useDeclaration(id);
 
