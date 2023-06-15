46ce2fca5f09a3b7e7349f21c53e3b43bf7fb529
farid <farid.boussetta.95@gmail.com>
2023-06-16 00:25:28 +0200

    feat(#551): add search declaration

--- back/src/api/declaration/index.ts
+++ back/src/api/declaration/index.ts
@@ -2,11 +2,7 @@ import { Router } from 'express';
 import { getDeclaration } from './getDeclaration';
 import { getSimulation } from './getSimulation';
 import { putDeclaration } from './putDeclaration';
-<<<<<<< HEAD
 import { patchStatus } from './patchStatus';
-
-export default Router().use(getSimulation).use(putDeclaration).use(getDeclaration).use(patchStatus);
-=======
 import { getDeclarationWithPublicId } from './getDeclarationWithPublicId';
 import { getDeclarations } from './getDeclarations';
 
@@ -15,5 +11,5 @@ export default Router()
   .use(putDeclaration)
   .use(getDeclaration)
   .use(getDeclarationWithPublicId)
-  .use(getDeclarations);
->>>>>>> 0e16980 (feat(#551): add search declaration)
+  .use(getDeclarations)
+  .use(patchStatus);
