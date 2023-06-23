2323a031c7ecbefe002e34122c0d86a78768a864
farid <farid.boussetta.95@gmail.com>
2023-06-23 15:27:17 +0200

    feat(#558): add list of declarations

--- front/src/components/business/FilterGroup/FilterGroup.stories.tsx
+++ front/src/components/business/FilterGroup/FilterGroup.stories.tsx
@@ -1,8 +1,10 @@
-import { Meta, Story } from '@storybook/react';
+import { useState } from 'react';
+
+import { Meta } from '@storybook/react';
 
 import { FilterHistoryItemProps } from '../FilterHistory';
 import { FilterBar, FilterBarProps } from './FilterBar';
-import { FilterGroup, FilterGroupProps } from './FilterGroup';
+import { FilterGroup } from './FilterGroup';
 import { FilterItem } from './FilterItem';
 import { DeclarationStatus } from '@/utils/declarationStatus.util';
 
@@ -19,64 +21,71 @@ const FILTER_HISTORY_ITEM_DATA: FilterHistoryItemProps = {
   to: 'http://www.google.fr',
 };
 
-const FILTER_DATA: FilterGroupProps = {
-  title: 'Statut de la quittance :',
-  filters: [
-    {
-      title: 'Payée',
-    },
-    {
-      title: 'En attente de paiement',
-    },
-    {
-      title: 'Annulée',
-    },
-  ],
-};
-
 const FILTER_BAR_DATA: FilterBarProps = {
   title: 'Plus de filtres',
   searchType: 'actuality',
-  filterGroups: [FILTER_DATA, FILTER_DATA, FILTER_DATA],
   filterHistories: [FILTER_HISTORY_ITEM_DATA, FILTER_HISTORY_ITEM_DATA, FILTER_HISTORY_ITEM_DATA],
   startDate: null,
   endDate: null,
   onSearch: () => {},
   onChangeDate: () => {},
   onValidateFilter: () => {},
-};
-
-export const Playground: Story<FilterGroupProps> = (args) => (
-  <FilterGroup title={args.title} filters={args.filters} />
-);
-
-Playground.args = {
-  title: FILTER_DATA.title,
-  filters: FILTER_DATA.filters,
+  activeFiltersMeanOfTransports: '',
+  activeFiltersStatus: '',
 };
 
 export const FilterBarExample = (): JSX.Element => (
   <FilterBar
     title={FILTER_BAR_DATA.title}
     searchType={FILTER_BAR_DATA.searchType}
-    filterGroups={FILTER_BAR_DATA.filterGroups}
     filterHistories={FILTER_BAR_DATA.filterHistories}
     startDate={FILTER_BAR_DATA.startDate}
     endDate={FILTER_BAR_DATA.endDate}
     onSearch={FILTER_BAR_DATA.onSearch}
     onChangeDate={FILTER_BAR_DATA.onChangeDate}
     onValidateFilter={FILTER_BAR_DATA.onValidateFilter}
+    activeFiltersMeanOfTransports={FILTER_BAR_DATA.activeFiltersMeanOfTransports}
+    activeFiltersStatus={FILTER_BAR_DATA.activeFiltersStatus}
   />
 );
 
-export const FilterGroupExample = (): JSX.Element => (
-  <FilterGroup title={FILTER_DATA.title} filters={FILTER_DATA.filters} />
-);
+export const FilterGroupExample = (): JSX.Element => {
+  const [activeFilters, setActiveFilters] = useState('');
+  return (
+    <FilterGroup
+      activeFilters={activeFilters}
+      filters={[
+        {
+          id: '1',
+          value: 'Filtre actif',
+        },
+        {
+          id: '2',
+          value: 'Filtre inactif',
+        },
+      ]}
+      title="Filtres"
+      onSelectFilter={() => setActiveFilters('Filtre actif')}
+    />
+  );
+};
 
 export const FilterItemExample = (): JSX.Element => (
   <div>
-    <FilterItem title="Filtre actif" isActive />
+    <FilterItem
+      filter={{
+        id: '1',
+        value: 'Filtre actif',
+      }}
+      activeFilters="1"
+    />
     <br />
-    <FilterItem title="Filtre inactif" />
+    <FilterItem
+      filter={{
+        id: '2',
+        value: 'Filtre inactif',
+      }}
+      activeFilters="Filtre actif"
+    />
   </div>
 );
