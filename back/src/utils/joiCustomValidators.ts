679ce8b050dc8f39317f07c243f59bc0a6efb874
farid <farid.boussetta.95@gmail.com>
2023-06-23 15:04:32 +0200

    feat(#558): add list of declarations

--- back/src/utils/joiCustomValidators.ts
+++ back/src/utils/joiCustomValidators.ts
@@ -1,24 +1,20 @@
 import { MeansOfTransport } from '../api/common/enums/meansOfTransport.enum';
 import { DeclarationStatus } from '../entities/declaration.entity';
 
-export function validateStatus(str: string): string {
-  const spl: string[] = str.split(',');
-  const newSpl = spl.filter((s) => s !== '');
-  for (const s of newSpl) {
-    if (!Object.values(DeclarationStatus).includes(s as DeclarationStatus)) {
-      throw new Error(`${s} is not a good status`);
-    }
-  }
-  return str;
-}
+export const validateStatus = (str: string): string => {
+  return validateListe(str, Object.values(DeclarationStatus));
+};
+export const validateMeanOfTransports = (str: string): string => {
+  return validateListe(str, Object.values(MeansOfTransport));
+};
 
-export function validateMeanOfTransports(str: string): string {
-  const spl: string[] = str.split(',');
+const validateListe = (str: string, arrayAllowed: string[]): string => {
+  const spl: string[] = str.trim().split(',');
   const newSpl = spl.filter((s) => s !== '');
   for (const s of newSpl) {
-    if (!Object.values(MeansOfTransport).includes(s.toLocaleLowerCase() as MeansOfTransport)) {
+    if (!arrayAllowed.includes(s as DeclarationStatus)) {
       throw new Error(`${s} is not a good status`);
     }
   }
   return str;
-}
+};
--- back/tests/api/declaration/getDeclarations/validator.test.ts
+++ back/tests/api/declaration/getDeclarations/validator.test.ts
@@ -9,6 +9,10 @@ const { isValid } = validatorHelper(validator);
 const queryParams = {
   searchPublicId: faker.string.nanoid(21),
   search: faker.string.nanoid(21),
+  status: ',validated,submitted',
+  meanOfTransports: ',car,train',
+  startDate: faker.date.past(),
+  endDate: faker.date.future(),
   limit: 10,
   offset: 0,
 };
@@ -55,6 +59,70 @@ describe('getDeclarations validator', () => {
       },
     };
     expect(isValid(validData)).toBe(false);
+  });
+  it('should not validate data meanOfTransports not good', () => {
+    const validData = {
+      query: {
+        ...queryParams,
+        meanOfTransports: ',train,submitted',
+      },
+    };
+    expect(isValid(validData)).toBe(false);
+  });
+  it('should validate proper data without meanOfTransports', () => {
+    const validData = {
+      query: {
+        ...queryParams,
+        meanOfTransports: undefined,
+      },
+    };
+    expect(isValid(validData)).toBe(true);
+  });
+  it('should not validate data status not good', () => {
+    const validData = {
+      query: {
+        ...queryParams,
+        status: ',train,submitted',
+      },
+    };
+    expect(isValid(validData)).toBe(false);
+  });
+  it('should validate proper data without status', () => {
+    const validData = {
+      query: {
+        ...queryParams,
+        status: undefined,
+      },
+    };
+    expect(isValid(validData)).toBe(true);
+  });
+  it('should not validate data endDate should be after startDate', () => {
+    const validData = {
+      query: {
+        ...queryParams,
+        endDate: faker.date.past(),
+        startDate: faker.date.future(),
+      },
+    };
+    expect(isValid(validData)).toBe(false);
+  });
+  it('should validate proper data without startDate', () => {
+    const validData = {
+      query: {
+        ...queryParams,
+        startDate: undefined,
+      },
+    };
+    expect(isValid(validData)).toBe(true);
+  });
+  it('should validate proper data without endDate', () => {
+    const validData = {
+      query: {
+        ...queryParams,
+        endDate: undefined,
+      },
+    };
+    expect(isValid(validData)).toBe(true);
   });
   it('should validate proper data without offset', () => {
     const validData = {
