85e1eb42ebe4da9275ff300c5fe7b911ac1f5ec4
farid <farid.boussetta.95@gmail.com>
2023-07-17 17:55:42 +0200

    feat(#569): add scanner
    feat(#569): add scanner
    
    feat(#569): add scanner
    
    feat(#570): add mail to put declaration rebase
    
    feat(#569): add scanner

--- front/src/components/input/StandardInputs/QrCodeScanner/QrCodeScanner.test.tsx
+++ front/src/components/input/StandardInputs/QrCodeScanner/QrCodeScanner.test.tsx
@@ -5,14 +5,15 @@ import { render } from '@testing-library/react';
 import { QrCodeScanner } from './QrCodeScanner';
 
 describe('QrCodeScanner', () => {
-  const onScanMock = jest.fn();
+  const onStartCamera = jest.fn();
+  const onStopCamera = jest.fn();
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   it('should render QR code scanner', async () => {
-    const wrapper = render(<QrCodeScanner onScan={onScanMock} />);
+    const wrapper = render(<QrCodeScanner startCamera={onStartCamera} stopCamera={onStopCamera} />);
     const video = wrapper.container.querySelector('#qr-reader-video');
 
     expect(video).toBeVisible();
