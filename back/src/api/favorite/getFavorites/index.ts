62b7ea82e779b1572e1c3d0dfde57e31b1a20d16
boussetta <farid_2312@hotmail.fr>
2023-12-14 12:23:23 +0100

    ajouts des favoris et gestion: add rate limiter

--- back/src/api/favorite/getFavorites/index.ts
+++ back/src/api/favorite/getFavorites/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { validatedExpressRequest } from '../../../core/utils/validatedExpressRequest';
 import { jwtMiddleware } from '../../../core/middlewares/jwt.middleware';
+import { RateLimiterType, rateLimiterMiddleware } from '../../../core/middlewares/rateLimiter';
 import route from './route';
 
 export const getFavorites = Router().get(
   '/favorite',
+  rateLimiterMiddleware({ type: RateLimiterType.DEFAULT }),
   jwtMiddleware({ isAgent: true }),
   validatedExpressRequest(route),
 );
