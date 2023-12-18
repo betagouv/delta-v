a0682211aa2ea0d2e54047e7ca8b158f64ba8eb4
Anthony HOANG <anthony.hoang@snowpact.com>
2023-12-18 14:42:07 +0100

    feat(search-product-history): remove console log

--- front/src/components/autonomous/ModalSearchProduct/ModalSearchProduct.tsx
+++ front/src/components/autonomous/ModalSearchProduct/ModalSearchProduct.tsx
@@ -21,7 +21,6 @@ export const ModalSearchProduct: React.FC<ModalSearchProductProps> = ({
   const { searchProducts } = useStore((state) => ({
     searchProducts: state.searchProducts,
   }));
-  console.log('searchProducts', searchProducts);
 
   return (
     <>
