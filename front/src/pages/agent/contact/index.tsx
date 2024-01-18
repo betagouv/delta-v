4d3079b690cb85bbc4ac2bb8eb128295624a97c9
boussetta <farid_2312@hotmail.fr>
2024-01-18 23:22:48 +0100

    Implementation du responsive Feedback

--- front/src/pages/agent/contact/index.tsx
+++ front/src/pages/agent/contact/index.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
+import { useMediaQuery } from 'react-responsive';
 import { v4 as uuidv4 } from 'uuid';
 import * as yup from 'yup';
 
 import { useCreateFeedbackMutation } from '@/api/hooks/useAPIFeedback';
 import { ModalValidateFeedbackInfo } from '@/components/autonomous/ModalValidateFeedbackInfo';
 import { AgentRoute } from '@/components/autonomous/RouteGuard/AgentRoute';
+import { Icon } from '@/components/common/Icon';
 import { TitleAgent } from '@/components/common/TitleAgent';
 import { Typography } from '@/components/common/Typography';
 import { InputGroup } from '@/components/input/InputGroup';
@@ -18,17 +20,20 @@ import { RoutingAgent } from '@/utils/const';
 
 export interface FormContactUsData {
   comment: string;
+  id: string;
 }
 
 const ContactPage = () => {
   const router = useRouter();
   const schema = yup.object({
     comment: yup.string().required('Le message est requis').min(10, 'Minimum 10 caractères'),
+    id: yup.string(),
   });
   const {
     handleSubmit,
     register,
     reset,
+    getValues,
     formState: { errors, isValid },
   } = useForm<FormContactUsData>({
     mode: 'onBlur',
@@ -38,6 +43,15 @@ const ContactPage = () => {
     },
   });
   const [openValidateFeedbackInfo, setOpenValidateFeedbackInfo] = useState(false);
+  const error = errors.id;
+
+  const [files, setFiles] = useState<File[]>(getValues('id') || []);
+
+  const dropzoneRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    error && dropzoneRef.current?.focus();
+  }, [error]);
 
   const onClickToRedirectToHome = () => {
     setOpenValidateFeedbackInfo(false);
@@ -60,6 +74,10 @@ const ContactPage = () => {
     });
   };
 
+  const isMobile = useMediaQuery({
+    query: '(max-width: 767px)',
+  });
+
   return (
     <AgentRoute>
       <MainAgent
@@ -72,6 +90,7 @@ const ContactPage = () => {
         withTitle
         titleHeader="Contact"
         linkButton={`${RoutingAgent.home}?mode=tools`}
+        isMobile={isMobile}
       >
         <form
           className="flex flex-col gap-4 py-6 px-4 justify-between flex-1"
@@ -99,6 +118,24 @@ const ContactPage = () => {
               />
             </div>
           </div>
+          <label
+            htmlFor="dropzone-file"
+            className="inline-flex border border-primary-600 text-primary-600 rounded-full px-5 py-2 justify-center items-center self-start"
+          >
+            <div className="inline-flex flex-row gap-1 items-center">
+              <Icon name="paperclip" size="sm" />
+              <Typography size="text-2xs" weight="bold">
+                Ajouter une pièce jointe
+              </Typography>
+            </div>
+            <input
+              id="dropzone-file"
+              name="dropzone-file"
+              ref={dropzoneRef}
+              type="file"
+              className="hidden"
+            />
+          </label>
           <div className="w-[118px] self-center mb-4">
             <button
               className={`py-3 w-full rounded-full  text-white ${
