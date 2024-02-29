67fb4764971331e9754515d0ef651042adac44df
Anthony HOANG <anthony.hoang@snowpact.com>
2024-02-29 12:50:33 +0100

    fix(recette): modify login errors

--- front/src/utils/errorFields.ts
+++ front/src/utils/errorFields.ts
@@ -3,9 +3,13 @@ import { IErrorResponse } from '@/api/lib/types';
 export const getErrorFields = (
   fieldName: string,
   apiError?: IErrorResponse,
+  genericBadCredentialError?: boolean,
 ): string | undefined => {
   if (!apiError) {
     return undefined;
+  }
+  if (apiError.statusCode === 401 && genericBadCredentialError) {
+    return 'Mail ou mot de passe invalide';
   }
   if (apiError.statusCode !== 400) {
     return undefined;
